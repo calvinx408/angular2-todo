c7cedaa9a39e1a75964f618856e9e1e53123a7cd
cbui <calvin_bui@intuit.com>
2017-03-04 21:31:57 -0800

    added more files

--- src/main.ts
+++ src/main.ts
@@ -2,6 +2,8 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { enableProdMode } from '@angular/core';
 import { environment } from './environments/environment';
 import { AppModule } from './app/app.module';
+import { bootstrap } from '@angular/platform-browser-dynamic';
+
 
 if (environment.production) {
   enableProdMode();
